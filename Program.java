679d39cd3ceb6326d06f66c1f7db9d5df6d41ac1
student <student@lab-44-8.cs.put.poznan.pl>
2019-11-04 14:02:13 +0100

    zmiana-1

--- Program.java
+++ Program.java
@@ -1,10 +1,34 @@
+/*komentarz drugi*/
 public class MyClass {
+	
+  int m = 9;
+  
+  int w=1;
+  
+	
+  int z = 10;
+  
+  int s=9;
+  
   int y = 7;
   int x = 5;
+  
+  int a=2;
 
   public static void main(String[] args) {
+	  
+	System.out.println("Ola");
+	int u = 5;
+	
     MyClass myObj = new MyClass();
     System.out.println(myObj.x);
+	int g = 0;
+	
+	System.out.println("Ola");
+	
+	System.out.println("Ola");
+	
+	System.out.println("Ola");
   }
 }
 
